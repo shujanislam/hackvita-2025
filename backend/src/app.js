2be9324d1a2521eb369c6da973cbbe3d59ee8f00
shujanislam <islamshujan231@gmail.com>
2025-03-30 22:17:51 +0530

    made the ai recommendation efficient + adding trending topics

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/auth.route.js");
 const roadmapRouter = require("./routes/roadmap.route.js");
 const testRouter = require("./routes/test.route.js");
 const recommendationRouter = require('./routes/recommendation.route.js');
+const trendingRouter = require('./routes/trending.route.js');
 
 dotenv.config();
 const app = express();
@@ -38,6 +39,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/roadmap", roadmapRouter);
 app.use("/api/v1/test", testRouter);
 app.use('/', recommendationRouter);
+app.use('/', trendingRouter);
 
 
 module.exports = app;
--- backend/src/routes/recommendation.route.js
+++ backend/src/routes/recommendation.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const { getRecommendationResponse } = require('../controllers/recommendation.controller.js');
 
-router.post(`/recommendation/:userId`, async (req, res)=>{
+router.get(`/recommendation/:userId`, async (req, res)=>{
   let userId = req.params.userId;
 
   let recommendedVideo = await getRecommendationResponse(userId);
