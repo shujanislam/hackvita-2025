8a1e8543d6a99859a6cbc4bcbc03abf35a69a1e7
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2025-03-30 20:06:40 +0530

    feat: generate content

--- backend/src/controllers/roadmap.controller.js
+++ backend/src/controllers/roadmap.controller.js
@@ -62,7 +62,59 @@ const structuredResponse = JSON.parse(rawText)
 return res.status(200).json({success:true, response: structuredResponse});
 }
 
+async function generateContent(req, res, next) {
+    const topic="Basic Concepts: System, Surroundings, Boundary";
+    const resSchema = {
+        type:Type.ARRAY,
+        items: {
+            type: Type.OBJECT,
+            properties: {
+                'heading': {
+                    type: Type.STRING,
+                    description: "heading of the subtopic",
+                    nullable: false
+                },
+                'lesson': {
+                    type: Type.STRING,
+                        description:'complete, detailed text lesson on the subtopic. add a fun fact at the end related to the subtopic',
+                        nullable:false
+                }
+            },
+            required: ["heading", "lesson"],
+            propertyOrdering:["heading", "lesson"]
+        }
+    };
+    
+    const prompt = `generate complete, detailed text lesson on the topic "${topic}". generate as many subtopics as needed.`;
+    
+    
+        
+    const response = await ai.models.generateContent({
+        model: 'gemini-2.0-flash',
+        contents: prompt,
+        config: {
+            responseMimeType: 'application/json',
+            responseSchema: resSchema,
+        },
+    });
+    
+    let rawText = response.text.trim();
+    
+    // Remove markdown code block (```json ... ```)
+    if (rawText.startsWith("```json")) {
+        rawText = rawText.slice(7, -3).trim();
+    } else if (rawText.startsWith("```")) {
+        rawText = rawText.slice(3, -3).trim();
+    }
+    
+    const structuredResponse = JSON.parse(rawText)
+    
+    
+    // console.debug(structuredResponse);
+    return res.status(200).json({success:true, response: structuredResponse});
+    }
 
-module.exports = {generateRoadmap};
+
+module.exports = {generateRoadmap, generateContent};
     
     
--- backend/src/routes/roadmap.route.js
+++ backend/src/routes/roadmap.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { protectRoute } = require('../middleware/auth.middleware');
-const { generateRoadmap } = require('../controllers/roadmap.controller');
+const { generateRoadmap, generateContent } = require('../controllers/roadmap.controller');
 
 router.post('/generate', generateRoadmap);
+router.post('/content', generateContent);
 router.post('/ping',(req,res)=>{
     res.status(200).json({success:true,message:"Pong"});
 })
